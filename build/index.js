20533cf7e7d3c6fff88cfceebfd6be84f63d406f
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-08-21 06:32:55 +0900

    Add Block Class

--- build/index.js
+++ build/index.js
@@ -1,8 +1,26 @@
-class Block {
-    constructor(data) {
-        this.data = data;
+class Dict {
+    constructor() {
+        this.words = {};
     }
-    static hello() {
-        return 'hi';
+    add(word) {
+        if (this.words[word.term] === undefined) {
+            this.words[word.term] = word.def;
+        }
+    }
+    def(term) {
+        return this.words[term];
+    }
+    del(term) {
+        delete this.words[term];
+    }
+    update(term, w) {
+        this.words[term] = w;
     }
 }
+class Word {
+    constructor(term, def) {
+        this.term = term;
+        this.def = def;
+    }
+}
+const kim = new Word('kim', 'food');
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
+import crypto from "crypto"
 
-class Block{
-  constructor(private data:string){
-    
+interface BlockShape{
+  hash:string;
+  prevHash:string;
+  height:number;
+  data:string;
+}
+
+class Block implements BlockShape{
+  public hash: string; 
+  constructor(
+    public prevHash:string,
+    public height:number,
+    public data:string,
+  ){
+    this.hash = Block.calculateHash(prevHash,height,data)
   }
-  static hello(){
-    return 'hi'
+  static calculateHash(prevHash:string,height:number,data:string){
+    const toHash = `${prevHash}${height}${data}`
+    return crypto.createHash("sha256").update(toHash).digest('hex')
   }
 }
