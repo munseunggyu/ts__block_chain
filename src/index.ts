10e367384ea130d79ec2669c049db4c0ae02fe6f
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-08-22 09:13:26 +0900

    Add BlockChain class

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,28 @@ class Block implements BlockShape{
     return crypto.createHash("sha256").update(toHash).digest('hex')
   }
 }
+
+class BlockChain{
+  private blocks: Block[]
+  constructor(){
+    this.blocks = []
+  }
+  private getPrevHash(){
+    if(this.blocks.length === 0) return ""
+    return this.blocks[this.blocks.length-1].hash
+  }
+  public addBlock(data:string){
+    const newBlock = new Block(this.getPrevHash(),this.blocks.length+1,data)
+    this.blocks.push(newBlock)
+  }
+  public getBlock(){
+    return [...this.blocks]
+  }
+}
+
+const blockChain = new BlockChain()
+blockChain.addBlock("First")
+blockChain.addBlock("Second")
+blockChain.addBlock("Three")
+
+console.log(blockChain)
